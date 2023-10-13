cb04a137f003439234296f96bbc02c6fccbacc76
AlekseyNikulin56 <nikulin_a.d@mail.ru>
2023-10-13 14:41:52 +0400

    add objects sumsung, iphone, xiaomi

--- HW11/HW11/src/Phone.java
+++ HW11/HW11/src/Phone.java
@@ -1,5 +1,5 @@
 public class Phone {
-
+    public String namePhone;
     public int numberPhone;
     public String modelPhone;
     public int weightPhone;
@@ -7,11 +7,18 @@ public class Phone {
 
     public Phone() {
     }
-    public Phone(int numberPhone, String modelPhone, int weightPhone, String countryMadePhone) {
+    public Phone(String namePhone, int numberPhone, String modelPhone, int weightPhone, String countryMadePhone) {
+        this.namePhone = namePhone;
         this.numberPhone = numberPhone;
         this.modelPhone = modelPhone;
         this.weightPhone = weightPhone;
         Phone.countryMadePhone = countryMadePhone;
+    }
+    public void receiveCall(String namePhone) {
+        System.out.println("Звонит абонент по имени " + namePhone);
+    }
+    public void receiveCall(String namePhone, int numberPhone) {
+        System.out.println("Звонит абонент по имени " + namePhone + "номер " + numberPhone);
     }
 
 }
